a812eab67317ae941dc2b40ba9d6b8f18b848c4b
joseph pelloquet <joseph.pelloquet@gmail.com>
2020-01-23 15:29:42 +0100

    debut features lanterne

---
+++ Assets/script/LanterneAction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanterneAction : MonoBehaviour
+{
+    private Animator myAnims;
+    private bool isLighting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnims = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && !isLighting)
+        {
+            myAnims.SetTrigger("ActiveMouvementLight");
+            isLighting = true;
+        }
+        if (Input.GetKey(KeyCode.A) && !isLighting)
+        {
+            myAnims.SetTrigger("StayLight");
+            isLighting = true;
+        }
+    }
+
+    public void StopLighting()
+    {
+        isLighting = false;
+    }
+}
